575bf6a9d8d6912a8f528b9094d1ac9d9439c055
Thomas Guyot <thomas.guyot@outlook.com>
2023-01-18 15:47:25 +0100

    ajout de joi-validator

---
+++ Backend/middleware/user.validator.js
@@ -0,0 +1,21 @@
+const joi = require("joi");
+
+const validation = joi.object({
+    email: joi.string().email().trim(true).required(),
+    password: joi.string().min(6).trim(true).required(),
+});
+
+const userValidation = async (req, res, next) => {
+	const payload = {
+		email: req.body.email,
+		password: req.body.password,
+	};
+
+	const { error } = validation.validate(payload);
+	if (error) {
+		res.status(406).json({message: `Error in User Data : ${error.message}`});
+	} else {
+		next();
+	}
+};
+module.exports = userValidation;
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
-const rateLimit = require('express-rate-limit')
+const rateLimit = require('express-rate-limit');
+const userValidation = require("../middleware/user.validator");
 
 const apiLimiter = rateLimit({
 	windowMs: 60 * 60 * 1000,
@@ -10,7 +11,7 @@ const apiLimiter = rateLimit({
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
 
-router.post('/signup', apiLimiter, userCtrl.signup)
+router.post('/signup', apiLimiter, userValidation, userCtrl.signup)
 router.post('/login', apiLimiter, userCtrl.login)
 
 module.exports = router;
